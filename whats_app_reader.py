10eaa293dea67cf6bc28c55513a1f2d0804c56df
VaibhavSiroha <vaibhavsiroha2000@gmail.com>
2021-10-11 21:00:48 +0530

    new comment added

--- whats_app_reader.py
+++ whats_app_reader.py
@@ -3,7 +3,7 @@ import pandas as pd
 from tkinter import * 
 from tkinter.filedialog import askopenfilename
 file_path = askopenfilename()
-
+#added another bogus comment
 with open(file_path, mode='r', encoding="utf8") as f:
     data = f.readlines()
 dataset = data[1:]
