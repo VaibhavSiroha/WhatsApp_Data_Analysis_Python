927744d225d41b044cd086c59254806464ac0c4d
VaibhavSiroha <vaibhavsiroha2000@gmail.com>
2021-10-11 21:03:25 +0530

    new code in the master branch

--- whats_app_reader.py
+++ whats_app_reader.py
@@ -3,7 +3,7 @@ import pandas as pd
 from tkinter import * 
 from tkinter.filedialog import askopenfilename
 file_path = askopenfilename()
-
+# I have added this comment to test something
 with open(file_path, mode='r', encoding="utf8") as f:
     data = f.readlines()
 dataset = data[1:]
